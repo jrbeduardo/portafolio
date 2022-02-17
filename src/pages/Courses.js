b566c01e5b242ddb32dc6f3662127c7789000b60
jrbeduardo <jrbeduardo@gmail.com>
2022-02-17 13:49:05 -0600

    update animations

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -9,9 +9,9 @@ export const Courses = () => {
     <>
       <div>
         <div className="h-screen relative">
-          <div className="h-3/4 pt-20 bg-cafe">
+          <div className="h-3/4 pt-20 bg-theme">
             <lottie-player
-              src="https://assets4.lottiefiles.com/packages/lf20_z01bika0.json"
+              src="https://assets4.lottiefiles.com/packages/lf20_zpymeot9.json" 
               background="transparent"
               speed="1"
               loop
@@ -21,7 +21,7 @@ export const Courses = () => {
           <div className="absolute left-0 right-0 -bottom-70">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
               <path
-                fill="#9e7a58"
+                fill="#2a2a2a"
                 d="M0,128L48,128C96,128,192,128,288,112C384,96,480,64,576,74.7C672,85,768,139,864,176C960,213,1056,235,1152,218.7C1248,203,1344,149,1392,122.7L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
               ></path>
             </svg>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,153 +1,252 @@
-import { FaDocker, FaFlask, FaGithub, FaHtml5, FaJava, FaNodeJs, FaPhp, FaPython, FaReact, FaUbuntu } from 'react-icons/fa';
-import { DiDjango } from 'react-icons/di';
-import { SiJavascript, SiMongodb, SiPostgresql, SiTailwindcss } from 'react-icons/si';
-import AOS from 'aos';
+import {
+  FaDocker,
+  FaFlask,
+  FaGithub,
+  FaHtml5,
+  FaJava,
+  FaNodeJs,
+  FaPhp,
+  FaPython,
+  FaReact,
+  FaUbuntu,
+} from "react-icons/fa";
+import { DiDjango } from "react-icons/di";
+import {
+  SiJavascript,
+  SiMongodb,
+  SiPostgresql,
+  SiTailwindcss,
+} from "react-icons/si";
+import AOS from "aos";
 import TypewriterComponent from "typewriter-effect";
 import { useScroll } from "../hooks/useScroll";
 AOS.init({
-  duration: 1000
+  duration: 1000,
 });
-
 
 export const Home = () => {
   useScroll();
-  return (    
-      <div>
-        {/* intro section */}
-        <div className='h-screen bg-theme'>
-          <div className='flex md:flex-col   justify-evenly h-screen items-center border-4 border-white rotate-12  mx-10 bg-theme md:rotate-0 md:border-0'>
-            <div className='h-3/5'>
-            <lottie-player src="https://assets2.lottiefiles.com/packages/lf20_q7uarxsb.json"  background="transparent"  speed="1"    loop autoplay></lottie-player>            
-            </div>
-            <div className='font-bold text-white md:px-5 border-y-white'>
-              <h1 className='text-5xl md:text-4xl'>Hola, soy <b className='text-yellow-500 '>José Eduardo</b></h1>
-              
-              <h2 className='text-3xl md:text-3xl text-red-500'>
-                <TypewriterComponent
+  return (
+    <div>
+      {/* intro section */}
+      <div className="h-screen bg-theme">
+        <div className="flex flex-col justify-evenly h-screen items-center border-4 border-white rotate-12  mx-10 bg-theme md:rotate-0 md:border-0">
+          <div className="h-2/3">
+            <lottie-player
+              src="https://assets4.lottiefiles.com/packages/lf20_aoqh4ohl.json"
+              background="transparent"
+              speed="1"
+              loop
+              autoplay
+            ></lottie-player>
+          </div>
+          <div className="font-bold text-white md:px-5 border-y-white">
+            <h1 className="text-5xl md:text-4xl">
+              Hola, soy <b className="text-yellow-500 ">José Eduardo</b>
+            </h1>
+
+            <h2 className="text-3xl md:text-3xl text-red-500">
+              <TypewriterComponent
                 options={{
-                  strings: ['Matemático', 
-                            'e<sup> i π</sup> = - 1',
-                            'Programador',
-                            '<code>print("hola Mundo")</code>'
-                          ],
+                  strings: [
+                    "Matemático",
+                    "e<sup> i π</sup> = - 1",
+                    "Programador",
+                    '<code>print("hola Mundo")</code>',
+                  ],
                   autoStart: true,
                   loop: true,
                 }}
-              /></h2>
-            </div>
-
+              />
+            </h2>
           </div>
-
-        </div>
-
-          {/* about */}  
-          <div className='h-screen text-xl font-raleway flex flex-col justify-center mx-20 md:mx-2.5 sm:mt-44 md:mt-30 mt-14 sm:mb-44'>
-            <h2 className='text-4xl text-center font-bold my-10 mt-0' data-aos='slide-right'>¿Quién Soy?</h2>
-            <p className='text-3xl md:text-2xl my-5'>
-              Estudié Matemáticas en la Universidad Nacional Autónoma de México (UNAM). Tengo la licenciatura y la maestría 🎓.
-            </p>
-            <p className='text-3xl md:text-2xl my-5'>
-              Desde que me gradue he impartido clases en distintas instituciones educativas.
-            </p>
-            <p className='text-3xl md:text-2xl my-5'>
-              Pero también disfruto y me apasiona la programación 💚.
-            </p>
-            <p className='text-3xl md:text-2xl my-5'>
-              En el desarrollo web 💻, estoy más orientado al Backend, pero también hago Frontend. También me gusta el análisis de datos 📄 y la inteligencia artificial 🤖.
-            </p>
-
-            <p className='text-3xl md:text-2xl my-5'>
-              Eterno Autodidacta.
-            </p>
-
-        </div>
-      
-        {/* Languages */}
-        <div>
-        <h2 className='text-4xl font-bold text-center  mb-10' data-aos='slide-right'>Lenguajes que manejo</h2>
-          <div className='grid md:grid-cols-1 grid-cols-4'>
-            <FaJava size={180} color="orange" className='w-full text-center mt-10'/>
-            <FaPython size={180} color="#048bb1" className='w-full text-center mt-10 '/>
-            <SiJavascript size={180} color="#e8d44d" className='w-full text-center mt-10 '/>
-            <FaPhp size={180} color="#6b75b2" className='w-full text-center mt-10'/>
-          </div>
-        </div>
-        {/* tecnologias */}
-        <div>
-          <h2 className='text-4xl font-bold text-center my-10' data-aos='slide-right'>Herramientas que uso</h2>
-          <div className='grid md:grid-cols-2 grid-cols-6'>
-            <FaReact size={160} color="#01c5f7" className='w-full text-center mt-10 animate-spin'/>
-            <DiDjango size={160} color="#103c2d" className='w-full text-center mt-10'/>
-            <FaFlask size={160}  className='w-full text-center mt-10'/>
-            <FaNodeJs size={160} color="#8cbf3e" className='w-full text-center mt-10'/>
-            <FaHtml5 size={160} color="#dd4b24" className='w-full text-center mt-10'/>
-            <SiPostgresql size={160} color="#305d8d" className='w-full text-center mt-10'/>
-            <SiMongodb size={160} color="#239443" className='w-full text-center mt-10'/>
-            <FaUbuntu size={160} color="#e55323" className='w-full text-center mt-10 '/>
-            <FaDocker size={160} color="#0996e5" className='w-full text-center mt-10'/>
-            <FaGithub size={160}  className='w-full text-center mt-10'/>
-            <SiTailwindcss size={160} color="#14b9c5" className='w-full text-center mt-10'/>
-          </div>
-        </div>
-        {/* Python Buff */}
-        <div className='my-20'>
-          <div className='text-center h-52 bg-azul'>
-            <h2 className='text-white font-bold text-3xl py-10'>Sí, estás en lo correcto... Soy fan de Python</h2>
-          </div>
-
-          <div className='md:mx-5 mx-32 shadow-2xl bg-gray-50 -mt-20 rounded-lg hover:bg-gray-700 hover:text-white'>
-            <div className='h-96'>
-            <lottie-player src="https://assets7.lottiefiles.com/packages/lf20_2znxgjyt.json"  background="transparent"  speed="1"   loop autoplay></lottie-player>
-            </div>
-            <p className='text-2xl my-5 md:text-xl md:px-5 px-14 py-10'>
-              Python es un lenguaje de programación de alto nivel que se utiliza para desarrollar aplicaciones de todo tipo. Se trata de un lenguaje multiplataforma de código abierto y, por lo tanto, gratuito, lo que permite desarrollar software sin límites.
-            </p>      
-          </div>
-          
-        </div>
-        {/* dev stack section */}
-        <div className='my-20'>
-          <div className='text-center h-52 bg-cafe'>
-            <h2 className='text-white font-bold text-3xl py-10'>My Dev stack</h2>
-          </div>
-
-          <div className='md:mx-5 mx-32 shadow-2xl bg-gray-50 -mt-20 rounded-lg hover:bg-gray-700 hover:text-white'>
-            <div className='h-96' data-aos='zoom-in-up'>
-            <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_sSF6EG.json"  
-            background="transparent"  
-            speed="1"   
-            loop 
-            autoplay></lottie-player>
-            </div>
-            <div className='grid md:grid-cols-1 grid-cols-3 p-5'>
-              <div className='text-left'>
-                <h2 className='text-xl font-bold'>FrontEnd</h2>
-                <hr></hr>
-                <p className='mt-2'>HTML/CSS</p>
-                <p className='mt-2'>React</p>
-                <p className='mt-2'>JavaScript</p>
-              </div>
-              <div className='text-center'>
-                <h2 className='text-xl font-bold'>UI / UX</h2>
-                <hr></hr>
-                <p className='mt-2'>Tailwind</p>
-                <p className='mt-2'>Bootstrap</p>
-              </div>
-              <div className='text-right'>
-                <h2 className='text-xl font-bold'>BackEnd and DB</h2>
-                <hr></hr>
-                <p className='mt-2'>Node JS</p>
-                <p className='mt-2'>Python</p>
-                <p className='mt-2'>MongoDB</p>
-                <p className='mt-2'>Oracle</p>
-              </div>
-            
-            </div>      
-          </div>
-          
         </div>
       </div>
 
-    
-  )
-}
+      {/* about */}
+      <div className="h-screen text-xl font-raleway flex flex-col justify-center mx-20 md:mx-2.5 sm:mt-44 md:mt-30 mt-14 sm:mb-44">
+        <h2
+          className="text-4xl text-center font-bold my-10 mt-0"
+          data-aos="slide-right"
+        >
+          ¿Quién Soy?
+        </h2>
+        <p className="text-3xl md:text-2xl my-5">
+          Estudié Matemáticas en la Universidad Nacional Autónoma de México
+          (UNAM). Tengo la licenciatura y la maestría 🎓.
+        </p>
+        <p className="text-3xl md:text-2xl my-5">
+          Desde que me gradue he impartido clases en distintas instituciones
+          educativas.
+        </p>
+        <p className="text-3xl md:text-2xl my-5">
+          Pero también disfruto y me apasiona la programación 💚.
+        </p>
+        <p className="text-3xl md:text-2xl my-5">
+          En el desarrollo web 💻, estoy más orientado al Backend, pero también
+          hago Frontend. También me gusta el análisis de datos 📄 y la
+          inteligencia artificial 🤖.
+        </p>
+
+        <p className="text-3xl md:text-2xl my-5">Eterno Autodidacta.</p>
+      </div>
+
+      {/* Languages */}
+      <div>
+        <h2
+          className="text-4xl font-bold text-center  mb-10"
+          data-aos="slide-right"
+        >
+          Lenguajes que manejo
+        </h2>
+        <div className="grid md:grid-cols-1 grid-cols-4">
+          <FaJava
+            size={180}
+            color="orange"
+            className="w-full text-center mt-10"
+          />
+          <FaPython
+            size={180}
+            color="#048bb1"
+            className="w-full text-center mt-10 "
+          />
+          <SiJavascript
+            size={180}
+            color="#e8d44d"
+            className="w-full text-center mt-10 "
+          />
+          <FaPhp
+            size={180}
+            color="#6b75b2"
+            className="w-full text-center mt-10"
+          />
+        </div>
+      </div>
+      {/* tecnologias */}
+      <div>
+        <h2
+          className="text-4xl font-bold text-center my-10"
+          data-aos="slide-right"
+        >
+          Herramientas que uso
+        </h2>
+        <div className="grid md:grid-cols-2 grid-cols-6">
+          <FaReact
+            size={160}
+            color="#01c5f7"
+            className="w-full text-center mt-10 animate-spin"
+          />
+          <DiDjango
+            size={160}
+            color="#103c2d"
+            className="w-full text-center mt-10"
+          />
+          <FaFlask size={160} className="w-full text-center mt-10" />
+          <FaNodeJs
+            size={160}
+            color="#8cbf3e"
+            className="w-full text-center mt-10"
+          />
+          <FaHtml5
+            size={160}
+            color="#dd4b24"
+            className="w-full text-center mt-10"
+          />
+          <SiPostgresql
+            size={160}
+            color="#305d8d"
+            className="w-full text-center mt-10"
+          />
+          <SiMongodb
+            size={160}
+            color="#239443"
+            className="w-full text-center mt-10"
+          />
+          <FaUbuntu
+            size={160}
+            color="#e55323"
+            className="w-full text-center mt-10 "
+          />
+          <FaDocker
+            size={160}
+            color="#0996e5"
+            className="w-full text-center mt-10"
+          />
+          <FaGithub size={160} className="w-full text-center mt-10" />
+          <SiTailwindcss
+            size={160}
+            color="#14b9c5"
+            className="w-full text-center mt-10"
+          />
+        </div>
+      </div>
+      {/* Python Buff */}
+      <div className="my-20">
+        <div className="text-center h-52 bg-azul">
+          <h2 className="text-white font-bold text-3xl py-10">
+            Sí, estás en lo correcto... Soy fan de Python
+          </h2>
+        </div>
+
+        <div className="md:mx-5 mx-32 shadow-2xl bg-gray-50 -mt-20 rounded-lg hover:bg-gray-700 hover:text-white">
+          <div className="h-96">
+            <lottie-player
+              src="https://assets7.lottiefiles.com/packages/lf20_2znxgjyt.json"
+              background="transparent"
+              speed="1"
+              loop
+              autoplay
+            ></lottie-player>
+          </div>
+          <p className="text-2xl my-5 md:text-xl md:px-5 px-14 py-10">
+            Python es un lenguaje de programación de alto nivel que se utiliza
+            para desarrollar aplicaciones de todo tipo. Se trata de un lenguaje
+            multiplataforma de código abierto y, por lo tanto, gratuito, lo que
+            permite desarrollar software sin límites.
+          </p>
+        </div>
+      </div>
+      {/* dev stack section */}
+      <div className="my-20">
+        <div className="text-center h-52 bg-cafe">
+          <h2 className="text-white font-bold text-3xl py-10">My Dev stack</h2>
+        </div>
+
+        <div className="md:mx-5 mx-32 shadow-2xl bg-gray-50 -mt-20 rounded-lg hover:bg-gray-700 hover:text-white">
+          <div className="h-96" data-aos="zoom-in-up">
+            <lottie-player
+              src="https://assets9.lottiefiles.com/packages/lf20_sSF6EG.json"
+              background="transparent"
+              speed="1"
+              loop
+              autoplay
+            ></lottie-player>
+          </div>
+          <div className="grid md:grid-cols-1 grid-cols-3 p-5">
+            <div className="text-left">
+              <h2 className="text-xl font-bold">FrontEnd</h2>
+              <hr></hr>
+              <p className="mt-2">HTML/CSS</p>
+              <p className="mt-2">React</p>
+              <p className="mt-2">JavaScript</p>
+            </div>
+            <div className="text-center">
+              <h2 className="text-xl font-bold">UI / UX</h2>
+              <hr></hr>
+              <p className="mt-2">Tailwind</p>
+              <p className="mt-2">Bootstrap</p>
+            </div>
+            <div className="text-right">
+              <h2 className="text-xl font-bold">BackEnd and DB</h2>
+              <hr></hr>
+              <p className="mt-2">Node JS</p>
+              <p className="mt-2">Python</p>
+              <p className="mt-2">MongoDB</p>
+              <p className="mt-2">Oracle</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
